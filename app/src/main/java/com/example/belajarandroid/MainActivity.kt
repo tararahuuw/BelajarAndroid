6f0e819e6cea463d7439c793cc4a8d5ec2d0be6e
tararahuuw <mfalamsyahofficial@gmail.com>
2023-09-22 01:18:00 +0700

    1. Gradle:Module 2. MainActivity.kt

--- app/src/main/java/com/example/belajarandroid/MainActivity.kt
+++ app/src/main/java/com/example/belajarandroid/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.belajarandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.belajarandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-	
+	private lateinit var binding: ActivityMainBinding
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_main)
+		binding = ActivityMainBinding.inflate(layoutInflater)
+		setContentView(binding.root)
 	}
 }
