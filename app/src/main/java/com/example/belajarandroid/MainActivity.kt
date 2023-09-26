3608d1f11cd93dea01bb208e807ca01f0c3160f9
tararahuuw <mfalamsyahofficial@gmail.com>
2023-09-26 07:16:28 +0700

    1. Create Fragment 2. MainActivity.kt

--- app/src/main/java/com/example/belajarandroid/MainActivity.kt
+++ app/src/main/java/com/example/belajarandroid/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.belajarandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.belajarandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 	
+	private lateinit var binding: ActivityMainBinding
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_main)
+		binding = ActivityMainBinding.inflate(layoutInflater)
+		setContentView(binding.root)
+		
+		val fragmentTransaction = supportFragmentManager.beginTransaction()
+		fragmentTransaction.add(R.id.frameLayout, ProfileFragment())
+		fragmentTransaction.commit()
 	}
 }
